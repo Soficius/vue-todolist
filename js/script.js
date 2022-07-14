24e40083d949ed1d7548458ef64c583397950a1c
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-14 15:55:16 +0200

    milestone 3 finito

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-
-
 console.log('vue ok', Vue)
 const root = new Vue({
     el: '#root',
     data:{
+        newTask:'',
         tasks:[
             {text:'fare la spesa', done: true},
             {text:'fare la valigia', done:false},
@@ -22,6 +14,15 @@ const root = new Vue({
     methods: {
         delateTask(index){
             this.tasks.splice(index,1)
+        },
+        addTask(){
+            if(this.newTask){
+                const newActivity ={ text: this.newTask, done: false};
+                this.tasks.push(newActivity);
+                this.newTask= ''
+            }
+            this.newTask.push(this.newTask)
+
         }
     }
 })
