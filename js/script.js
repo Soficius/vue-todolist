dcccccaf260f9282a89ff2659a2284407e627955
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-14 13:09:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('vue ok', Vue)
+const root = new Vue({
+    el: '#root',
+    data:{
+        tasks:[
+            'fare la spesa',
+            'fare la valigia',
+            'lavarsi i denti',
+            'andare in palestra',
+            'fare da mangiare',
+        ]
+    }
+})
